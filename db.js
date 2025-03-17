0437adbe2cf0327dc17cc310dfa6dfaa0bc92017
barbalanga <barbalanga1@gmail.com>
2025-03-17 16:43:36 +0200

    change

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'barby100',
+  password: 'password',
   database: 'my_recipes',
   waitForConnections: true,
   connectionLimit: 10,
